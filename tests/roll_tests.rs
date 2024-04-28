bc8d9fd4fb2eee4eb70daccb799a12587a847769
Jonathan Yaeger <jmyaeger@gmail.com>
2024-04-28 01:27:15 -0400

    Add basic melee roll tests

--- tests/roll_tests.rs
+++ tests/roll_tests.rs
@@ -1,12 +1,13 @@
-use osrs::equipment::{Armor, Weapon};
+use osrs::equipment::{Armor, CombatStyle, CombatType, Weapon};
 use osrs::monster::Monster;
 use osrs::player::{Gear, Player, PlayerStats};
 use osrs::potions::{Potion, PotionBoost};
+use osrs::prayers::{Prayer, PrayerBoost};
 use osrs::rolls::{
     calc_player_def_rolls, calc_player_magic_rolls, calc_player_melee_rolls,
     calc_player_ranged_rolls,
 };
-use rstest::fixture;
+use rstest::{fixture, rstest};
 
 #[fixture]
 fn max_melee_player() -> Player {
@@ -20,6 +21,7 @@ fn max_melee_player() -> Player {
         hitpoints: 99,
         prayer: 99,
     };
+    player.prayers.add(PrayerBoost::new(Prayer::Piety));
     player.potions.attack = Some(PotionBoost::new(Potion::SuperAttack));
     player.potions.strength = Some(PotionBoost::new(Potion::SuperStrength));
     player.potions.defence = Some(PotionBoost::new(Potion::SuperDefence));
@@ -41,6 +43,7 @@ fn max_melee_player() -> Player {
         ring: Some(Armor::new("Ultor ring")),
     };
     player.update_bonuses();
+    player.set_active_style(CombatStyle::Lunge);
 
     player
 }
@@ -57,6 +60,7 @@ fn mid_level_melee_player() -> Player {
         hitpoints: 80,
         prayer: 70,
     };
+    player.prayers.add(PrayerBoost::new(Prayer::Piety));
     player.potions.attack = Some(PotionBoost::new(Potion::SuperAttack));
     player.potions.strength = Some(PotionBoost::new(Potion::SuperStrength));
     player.potions.defence = Some(PotionBoost::new(Potion::SuperDefence));
@@ -78,6 +82,25 @@ fn mid_level_melee_player() -> Player {
         ring: Some(Armor::new("Berserker ring (i)")),
     };
     player.update_bonuses();
+    player.set_active_style(CombatStyle::Lash);
 
     player
+}
+
+#[rstest]
+fn test_max_melee_player_rolls(mut max_melee_player: Player) {
+    let monster = Monster::new("Ammonite Crab").unwrap();
+    calc_player_melee_rolls(&mut max_melee_player, &monster);
+
+    assert_eq!(max_melee_player.att_rolls[&CombatType::Stab], 33525);
+    assert_eq!(max_melee_player.max_hits[&CombatType::Stab], 56);
+}
+
+#[rstest]
+fn test_mid_level_melee_player_rolls(mut mid_level_melee_player: Player) {
+    let monster = Monster::new("Ammonite Crab").unwrap();
+    calc_player_melee_rolls(&mut mid_level_melee_player, &monster);
+
+    assert_eq!(mid_level_melee_player.att_rolls[&CombatType::Slash], 24125);
+    assert_eq!(mid_level_melee_player.max_hits[&CombatType::Stab], 39);
 }
